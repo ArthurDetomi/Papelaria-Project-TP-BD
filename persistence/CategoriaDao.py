c26638bf07513003f81bd4d4af92b14b5f1117b0
Arthur Detomi <arthurdetomi@gmail.com>
2025-02-06 19:56:59 -0300

    release: More Dao implemented

---
+++ persistence/CategoriaDao.py
@@ -0,0 +1,48 @@
+from persistence.Database import Database
+from model.Categoria import Categoria
+
+class CategoriaDao:
+
+    def find_all(self):
+        with Database() as db:
+            result = db.query("SELECT * FROM categoria", fetch=True)
+
+        categorias = []
+
+        for data in result:
+            id_, nome, cadastrado, editado = data
+
+            fp = Categoria(id=id_, nome=nome, cadastrado=cadastrado, editado=editado)
+
+            categorias.append(fp)
+
+
+        return categorias
+
+    def find_by_id(self, id):
+        with Database() as db:
+            result = db.query("SELECT * FROM categoria WHERE id = %s", params=(id,), fetch_one=True)
+
+        if result is None:
+            return None
+
+        id_, nome, cadastrado, editado = result
+
+        return Categoria(id=id_, nome=nome, cadastrado=cadastrado, editado=editado)
+
+    def save(self, categoria : Categoria):
+        with Database() as db:
+            result = db.query("INSERT INTO categoria (nome) values (%s)", (categoria.nome,))
+        return result
+
+    def delete(self, id):
+        with Database() as db:
+            result = db.query("DELETE FROM categoria WHERE id = %s", (id,))
+        return result
+
+    def update(self, categoria : Categoria):
+        with Database() as db:
+            result = db.query("UPDATE categoria SET nome=%s WHERE id = %s", (categoria.nome, categoria.id,))
+        return result
+
+
--- persistence/ClienteDao.py
+++ persistence/ClienteDao.py
@@ -1,48 +1,46 @@
 from persistence.Database import Database
 from model.Cliente import Cliente
-from persistence.GenericDao import GenericDao
 
 class ClienteDao():
-    
+
     def find_all(self):
         with Database() as db:
             result = db.query("SELECT * FROM cliente", fetch=True)
-        
+
         clientes = []
-        
+
         for data in result:
             id_, nome, cpf, telefone, cadastrado, editado = data
-            
+
             client = Cliente(id=id_, nome=nome, telefone=telefone, cpf=cpf, cadastrado=cadastrado, editado=editado)
-            
+
             clientes.append(client)
-            
-        
+
+
         return clientes
 
-    def find_by_id(self, id):        
+    def find_by_id(self, id):
         with Database() as db:
             result = db.query("SELECT * FROM cliente WHERE id = %s", params=(id,), fetch_one=True)
-        
+
         if result is None:
             return None
-        
+
         id_, nome, cpf, telefone, cadastrado, editado = result
-        
+
         return Cliente(id=id_, nome=nome, telefone=telefone, cpf=cpf, cadastrado=cadastrado, editado=editado)
-    
+
     def save(self, cliente : Cliente):
         with Database() as db:
             result = db.query("INSERT INTO cliente (nome, cpf, telefone) values (%s, %s, %s)", (cliente.nome, cliente.cpf, cliente.telefone,))
         return result
-    
+
     def delete(self, id):
         with Database() as db:
             result = db.query("DELETE FROM cliente WHERE id = %s", (id,))
         return result
-    
+
     def update(self, cliente : Cliente):
         with Database() as db:
             result = db.query("UPDATE cliente SET nome=%s, cpf=%s, telefone=%s WHERE id = %s", (cliente.nome,cliente.cpf,cliente.telefone,cliente.id,))
         return result
-    
