4c2d9a5b70107f494b40e1c15a87a64f3f6f1cf1
Arthur Detomi <arthurdetomi@gmail.com>
2025-02-07 17:52:23 -0300

    fix: Removing unnecessary things

--- util/DataBaseConfig.py
+++ util/DataBaseConfig.py
@@ -1,24 +1,24 @@
 class DatabaseConfig:
     _instancia = None
-    
-    def __init__(self): 
+
+    def __init__(self):
         properties = self.read_properties()
         self.database = properties["database"]
         self.host = properties["host"]
         self.user = properties["user"]
         self.password = properties["password"]
         self.port = properties["port"]
-        
+
     def read_properties(self):
         properties = {}
         with open('resources/db.properties', encoding="utf-8") as f:
             for line in f:
                 str = line.split("=")
-                properties[str[0]] = str[1].rstrip("\n")
-                            
-        return properties 
-            
-        
+                properties[str[0]] = str[1].rstrip()
+
+        return properties
+
+
     def __new__(cls, *args, **kwargs):
         if not cls._instancia:
             cls._instancia = super().__new__(cls, *args, **kwargs)
